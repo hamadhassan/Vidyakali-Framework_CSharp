5724c319712c0c264df15c2b2d152d2c7c1e342f
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-06-18 06:19:39 +0500

    Setup Environment

---
+++ VidyakaliFramework/Consumer/frmMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Framework.Core;
+using Framework.Collision;
+using Framework.Movement;
+
+
+namespace Consumer
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
